d875379e16e00cf81a676f65cedabea9de76f475
Barada123 <2361701js@gmail.com>
2016-12-20 02:40:30 +0700

    site_ZZZ24

---
+++ test_page.php
@@ -0,0 +1,11 @@
+<?php
+Header("Access-Control-Allow-Origin: *");
+if(isset($_POST['text'])){
+    echo $_POST['text'];
+}else{
+    echo "Ytn lfyysнвава";
+    echo "Ytn lfyysнвава";
+    echo "Ytn lfyysнвава";
+    echo "Ytn lfyysнвава";
+}
+?>
